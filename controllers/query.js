55ba85a9127fd98a59dbf2a6697da582f3753ee7
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2021-12-02 19:14:36 +0530

    route and controller updated

--- controllers/query.js
+++ controllers/query.js
@@ -1,5 +1,6 @@
 const Query = require("../models/query");
 
+//ADD QUERY
 exports.addQuery = async (req, res) => {
   console.log(req.body);
   const queryObj = new Query(req.body);
@@ -17,4 +18,39 @@ exports.addQuery = async (req, res) => {
       error: error.message,
     });
   }
+};
+
+//GET USER BY ID
+exports.getUserById = (req, res, next, id) => {
+  Query.findById(id).exec((err, user) => {
+    if (err || user) {
+      return res.status(400).json({
+        error: "No User Found",
+      });
+    }
+    res.profile = user;
+    next();
+  });
+};
+
+//GET USER
+exports.getUser = (req, res) => {
+  return res.json(req.profile);
+};
+
+//UPDATE USER
+exports.updateUser = (req, res) => {
+  Query.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized for updating the info",
+        });
+      }
+      res.json(user);
+    }
+  );
 };
--- routes/query.js
+++ routes/query.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addQuery } = require("../controllers/query");
+const {
+  addQuery,
+  getUserById,
+  getUser,
+  updateUser,
+} = require("../controllers/query");
 const { body, validationResult } = require("express-validator");
 
 const validation = [
@@ -36,6 +41,16 @@ const validation = [
   body("customerFeedback", ""),
 ];
 
+//PARAMETER ROUTE
+router.param("userId", getUserById);
+
+//WRITE ROUTE
 router.post("/addQuery", validation, addQuery);
+
+//READ ROUTE
+router.get("/user/:userId", getUser);
+
+//UPDATE USER
+router.put("/user/:userId", updateUser);
 
 module.exports = router;
